53dc49efbf051067486800b7be1a8d5d24b8f19d
Filipe Santiago <filipe@upplify.com>
2020-08-25 22:37:41 -0300

    add glew

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -18,6 +20,12 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    // Glew must be initialized after OpenGL context be created
+    if (glewInit() != GLEW_OK)
+        std::cout << "GlEW ERROR" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
